20ca4e66fea31ede61eb7d0586bebb9a98d593dc
souhair <youmbaisouhair@gmail.com>
2026-03-08 21:26:10 +0100

    tp1 completed

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -23,11 +23,17 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
-        Book(isbn = "", title = "Clean Code", nbPages = 0),
-        Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
-        Book(isbn = "", title = "Design Patterns", nbPages = 0),
-        Book(isbn = "", title = "Refactoring", nbPages = 0),
-        Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+        Book(isbn = "978-0-13-235088-4", title = "Clean Code", nbPages = 464),
+        Book(isbn = "978-0-13-595705-9", title = "The Pragmatic Programmer", nbPages = 352),
+        Book(isbn = "978-0-201-63361-0", title = "Design Patterns", nbPages = 395),
+        Book(isbn = "978-0-201-48567-7", title = "Refactoring", nbPages = 431),
+        Book(isbn = "978-0-596-00712-6", title = "Head First Design Patterns", nbPages = 694),
+
+        Book(isbn = "978-0-262-03384-8", title = "Introduction to Algorithms", nbPages = 1312),
+        Book(isbn = "978-0-13-110362-7", title = "The C Programming Language", nbPages = 288),
+        Book(isbn = "978-0-13-468599-1", title = "Effective Java", nbPages = 416),
+        Book(isbn = "978-0-596-52068-7", title = "Learning SQL", nbPages = 336),
+        Book(isbn = "978-1-491-92424-6", title = "Kotlin in Action", nbPages = 360)
     )
 
     /**
